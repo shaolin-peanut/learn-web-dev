876ad4a1015e21a75696ceae1d1691d0064b9c29
shaolin-peanut <shaolinpeanut@gmail.com>
2023-06-16 16:58:55 +0200

    nest js tutorial added

---
+++ ninja-api/src/ninjas/ninjas.controller.ts
@@ -0,0 +1,34 @@
+import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
+import { CreateNinjaDto } from './dto/create-ninja.dto';
+import { UpdateNinjaDto } from './dto/update-ninja.dto';
+import { NinjasService } from './ninjas.service';
+
+@Controller('ninjas')
+export class NinjasController {
+    constructor(private readonly ninjasService: NinjasService) {}
+
+    @Get()
+    getNinjas(@Query('weapon') weapon: 'stars' | 'nunchucks') {
+        return this.ninjasService.getNinjas(weapon);
+    }
+
+    @Get(':id')
+    getOneNinja(@Param('id') id: string) {
+        return this.ninjasService.getNinja(+id);
+    }
+
+    @Post()
+    createNinja(@Body() createNinjaDto: CreateNinjaDto) {
+        return this.ninjasService.createNinja(createNinjaDto);
+    }
+
+    @Put(':id')
+    updateNinja(@Param('id') id: string, @Body() updateNinjaDto: UpdateNinjaDto) {
+        return this.ninjasService.updateNinjas(+id, updateNinjaDto);
+    }
+    
+    @Delete(':id')
+    deleteNinja(@Param('id') id: string) {
+        return this.ninjasService.removeNinja(+id);
+    }
+}
